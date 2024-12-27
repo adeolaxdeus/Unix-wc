ad3b2d2d4f582e92f4812328bcf33fecd9795fb9
adeolaxdeus <adeolaxdeus@gmail.com>
2024-12-27 01:08:25 -0800

    support 'command filename' option

--- ccwc.c
+++ ccwc.c
@@ -6,15 +6,21 @@
  * @ac: Number of command line argument passed
  * @av:  Pointer to to command line arguments
  *
- * Return: 0 on success otherwise -1 
+ * Return: 0 on success otherwise -1
  */
 int main(int ac, char **av)
 {
 	const char *filename = av[2];
 	const char *option = av[1];
 	ssize_t (*func_ptr)(const char *);
-	(void) ac;
 
+	if (ac == 2 && av[1] != NULL)
+	{
+		filename = av[1];
+		printf(" %lu %lu %lu %s\n", line_count(filename), word_count(filename),
+				byte_count(filename), filename);
+		return (0);
+	}
 	func_ptr = get_op_func(option);
 	if (func_ptr == NULL)
 	{
