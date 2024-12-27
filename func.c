19689c01d213e1ba0e40399f9fb43eace850a2b1
adeolaxdeus <adeolaxdeus@gmail.com>
2024-12-26 22:45:18 -0800

    add -w option

--- func.c
+++ func.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,6 +28,7 @@ int byte_count(const char *filename)
 	if (file_size == -1)
 	{
 		perror("lseek");
+		close(fd);
 		return (-1);
 	}
 	printf("\t%lu %s\n", file_size, filename);
@@ -47,9 +49,9 @@ int byte_count(const char *filename)
 int line_count(const char *filename)
 {
 	int fd;
-	size_t newline_count = 0;
+	size_t newline_count = 0, i = 0;
 	char *buffer;
-	ssize_t numread, i = 0;
+	ssize_t numread;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -59,11 +61,14 @@ int line_count(const char *filename)
 	}
 	buffer = malloc(BUF_SIZE * sizeof(char));
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (-1);
+	}
 	/* read a byte at a time */
 	while ((numread = read(fd, buffer, BUF_SIZE)) > 0)
 	{
-		while (i < numread)
+		while (i < (size_t)numread)
 		{
 			if (buffer[i] == '\n') /* check if current byte is a '\n' character */
 				newline_count++;
@@ -79,6 +84,70 @@ int line_count(const char *filename)
 	}
 	free(buffer);
 	printf("\t%lu %s\n", newline_count, filename);
+	if (close(fd) == -1)
+	{
+		perror("close");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * word_count - Find the number of words in a file
+ * @filename: File to be read
+ *
+ * Return: Number of words in a file on success otherwise -1 on failure
+ */
+int word_count(const char *filename)
+{
+	int fd, in_word = 0;
+	char *buffer;
+	size_t word_num = 0, i = 0;
+	ssize_t numread;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (-1);
+	}
+	buffer = malloc(BUF_SIZE * sizeof(char));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	while ((numread = read(fd, buffer, BUF_SIZE)) > 0)
+	{
+		while (i < (size_t)numread)
+		{
+			if (isspace(buffer[i]))
+			{
+				if (in_word)
+				{
+					word_num++;
+					in_word = 0; /* End of word */
+				}
+			}
+			else
+			{
+				in_word = 1; /* inside a word */
+			}
+			i++;
+		}
+		i = 0;
+	}
+	/*  Account for last word in a file */
+	if (in_word)
+		word_num++;
+	if (numread == -1)
+	{
+		perror("read");
+		free(buffer);
+		return (-1);
+	}
+	free(buffer);
+	printf("\t%lu %s\n", word_num, filename);
 	if (close(fd) == -1)
 	{
 		perror("close");
